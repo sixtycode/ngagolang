824e02976ac902dad77230b5b1bbb0b7d9a06473
vocversefrontend <96647754+sixtycode@users.noreply.github.com>
2024-01-02 19:15:13 +0700

    Update main.go

--- case001/main.go
+++ case001/main.go
@@ -1,7 +1,33 @@
+// This is the name of our package
+// Everything with this package name can see everything
+// else inside the same package, regardless of the file they are in
 package main
 
-import "fmt"
+// These are the libraries we are going to use
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/template"
+)
 
-func main() {
-	fmt.Println("Hello, World!")
+// Declare type pointer to a template
+var tmpl *template.Template
+
+// Using the init function to make sure the template is only parsed once in the program
+func init() {
+	// template.Must takes the reponse of template.ParseFiles and does error checking
+	tmpl = template.Must(template.ParseFiles("templates/template.html"))
 }
+
+// Parse all the files in a certain directory
+func main() {
+	fmt.Println("Hello, World!\n")
+	// Execute myName into the template and print to Stdout
+	myName := "Fajrin"
+	myTeam := "Vio"
+	err := tmpl.Execute(os.Stdout, myName, myTeam)
+	if err != nil {
+		log.Fatalln(err)
+	}
+} // end main
