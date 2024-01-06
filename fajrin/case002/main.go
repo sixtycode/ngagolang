7712c44602f2e8615fd8013d991c3e6d75c8bd2a
vocversefrontend <96647754+sixtycode@users.noreply.github.com>
2024-01-06 10:56:34 +0700

    Update main.go

--- fajrin/case002/main.go
+++ fajrin/case002/main.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
+type orang struct {
+	nama string
+	umur int
+}
+
+type hewan struct {
+	nama string
+	umur int
+}
+
+type mahlukHidup interface {
+}
+
+func berbicara() {
+
+}
+
+func speak(m mahlukHidup) {
+
+}
+
 func main() {
+	fmt.Println("Halo Dunia")
 
 }
