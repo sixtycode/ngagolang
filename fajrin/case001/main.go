8e34f4208f450221cadf1c601d7bf6e246d7a85f
vocversefrontend <96647754+sixtycode@users.noreply.github.com>
2024-01-06 10:42:54 +0700

    Update main.go

--- fajrin/case001/main.go
+++ fajrin/case001/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+type identitas struct {
+	nama   string
+	umur   int
+	alamat string
+}
+
+func main() {
+	data := []identitas{
+		{"Ujang", 21, "Jl. Indihiang Raya No. 1 Kota Tasikmalaya"},
+		{"Udin", 24, "Jl. Cihideung Asri No. 37 Kota Tasikmalaya"},
+		{"Usep", 22, "Jl. Cipatujah Resort No. 7 Kabupaten Tasikmalaya"},
+	}
+
+	for _, i := range data {
+		fmt.Println("\nHalo nama saya", i.nama, ", umur saya",
+			i.umur, "dan saya tinggal di", i.alamat, "\n")
+	}
 }
