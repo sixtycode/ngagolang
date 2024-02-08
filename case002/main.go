658ec70d528f0c8375be5317808d0706893d69e1
sixtycode <96647754+sixtycode@users.noreply.github.com>
2024-02-08 19:47:59 +0700

    Update main.go

--- case002/main.go
+++ case002/main.go
@@ -1,17 +1,11 @@
-// This is the name of our package
-// Everything with this package name can see everything
-// else inside the same package, regardless of the file they are in
 package main
 
-// These are the libraries we are going to use
 import (
-	"fmt"
 	"log"
 	"os"
 	"text/template"
 )
 
-// Declare type pointer to a template
 var tmpl *template.Template
 
 type StrukturData struct {
@@ -19,32 +13,23 @@ type StrukturData struct {
 	Nama string
 }
 
-// Using the init function to make sure the template is only parsed once in the program
 func init() {
-	// template.Must takes the reponse of template.ParseFiles and does error checking
 	tmpl = template.Must(template.ParseGlob("templates/*"))
 }
 
-// Parse all the files in a certain directory
 func main() {
-	fmt.Println("Hello, World!\n")
-	// Execute myName into the template and print to Stdout
 	data := []StrukturData{
-		StrukturData{
-			id:   1,
-			Nama: "Udin",
-		},
-		StrukturData{
-			id:   2,
-			Nama: "Sasmita",
-		},
-		StrukturData{
-			id:   3,
-			Nama: "Ujang",
-		},
+		{1, "Usep"},
+		{2, "Sasmita"},
+		{3, "Uceng"},
+		{4, "Anto"},
+		{5, "Rojak"},
+		{6, "Saepul"},
+		{7, "Roki"},
 	}
-	err := tmpl.ExecuteTemplate(os.Stdout, "index.gohtml", &data)
+
+	err := tmpl.ExecuteTemplate(os.Stdout, "index", data)
 	if err != nil {
 		log.Fatalln(err)
 	}
-} // end main
+}
