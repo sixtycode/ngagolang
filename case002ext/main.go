15c99e3f586c279ebc4a2d2eb1bbb08092a1c3f9
vocversefrontend <96647754+sixtycode@users.noreply.github.com>
2024-02-21 14:20:03 +0700

    Update main.go

--- case002ext/main.go
+++ case002ext/main.go
@@ -9,8 +9,9 @@ import (
 var tmpl *template.Template
 
 type StrukturData struct {
-	Id   int
-	Nama string
+	Id    int
+	Nama  string
+	Nilai float32
 }
 
 func init() {
@@ -19,13 +20,20 @@ func init() {
 
 func main() {
 	data := []StrukturData{
-		{1, "Usep"},
-		{2, "Sasmita"},
-		{3, "Uceng"},
-		{4, "Anto"},
-		{5, "Rojak"},
-		{6, "Saepul"},
-		{7, "Roki"},
+		{1, "Usep Karawita", 73.7},
+		{2, "Sasmita Sumbul", 81.4},
+		{3, "Uceng Kawazaki", 83.6},
+		{4, "Anto Estriver", 44.1},
+		{5, "Rojak Kago", 62.9},
+		{6, "Saepul Khidir", 91.6},
+		{7, "Roki Khasmiri", 84.9},
+		{8, "Ismail Kanabawi", 66.6},
+		{9, "El Gato", 94.4},
+		{10, "Thomas Katt", 53.8},
+		{11, "Antent Zaslavski", 88.3},
+		{12, "Senn Cross", 80.0},
+		{13, "Alvin Nathanael", 69.2},
+		{14, "Samantha D. Larsson", 71.5},
 	}
 
 	err := tmpl.ExecuteTemplate(os.Stdout, "index", data)
